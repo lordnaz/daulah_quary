599988b3ab779fb09fef28fe217696d52b2bde20
SazrulNizam <sazrulnizam999@gmail.com>
2021-09-26 15:59:03 +0800

    latest

---
+++ app/Http/Controllers/ConsumptionInventoryController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConsumptionInventoryController extends Controller
+{
+    //
+}
---
+++ database/migrations/2021_09_25_112626_create_itemout_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemoutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itemout', function (Blueprint $table) {
+            $table->id('itemout_id');
+            $table->integer('quantityout')->nullable();
+            $table->foreignId('table_id');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itemout');
+    }
+}
