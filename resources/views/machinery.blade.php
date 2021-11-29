f04b1ff95ebe833c97bdd6d56526f2ddaae36803
Nazrul Hanif <nazrul.harith@mdec.com.my>
2021-11-29 13:09:31 +0800

    adding serial number in table machine

--- resources/views/machinery.blade.php
+++ resources/views/machinery.blade.php
@@ -94,7 +94,7 @@
                     <thead>
                         <tr
                             class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                            
+                            <th class="px-4 py-3">Serial Number</th>
                             <th class="px-4 py-3">Name</th>
                             <th class="px-4 py-3">Created By</th>
                             <th class="px-4 py-3">Type</th>
@@ -107,6 +107,9 @@
                     <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                         @foreach ($items as $item)
                         <tr class="text-gray-700 dark:text-gray-400">
+                        <td class="px-4 py-3 text-sm">
+                            {{$item->SerialNum}}
+                            </td>
                             <td class="px-4 py-3 text-sm">
                             {{$item->item_name}}
                             </td>
